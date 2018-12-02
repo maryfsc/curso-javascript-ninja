665c3b263a16191524ef93304121ff2d41647fe7
Mary Francis <maryfsc@disroot.org>
2018-12-01 23:00:00 -0200

    Exercício resolvido.

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'Super Campeões';
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,9 +13,8 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
-
-console.log( 'Times que estão participando do campeonato:', teams );
+var teams = ['Time do Akira', 'Time da Saori', 'Time do Naruto', 'Time do Tsubasa', 'Time da Sakura'];
+console.log( 'Times que estão participando do campeonato: ', teams );
 
 /*
 Crie uma função chamada `showTeamPosition` com as seguintes características:
@@ -32,19 +32,47 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition(position) {
+    switch(position) {
+        case 1: 
+        console.log('O time que está em 1ª posição é o ' + teams[0] + '.');
+        break;
+        case 2:
+        console.log('O time que está em 2ª posição é o ' + teams[1] + '.');
+        break;
+        case 3:
+        console.log('O time que está em 3ª posição é o ' + teams[2] + '.');
+        break;
+        case 4:
+        console.log('O time que está em 4ª posição é o ' + teams[3] + '.');
+        break;
+        case 5:
+        console.log('O time que está em 5ª posição é o ' + teams[4] + '.');
+        break;
+        default:
+        console.log('Não temos a informação do time que está nessa posição.');        
+    }
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+showTeamPosition(3); // O time que está em 3ª posição é o Time do Naruto.
+showTeamPosition(4); // O time que está em 4ª posição é o Time do Tsubasa.
+showTeamPosition(5); // O time que está em 5ª posição é o Time da Sakura.
+showTeamPosition(10); // Não temos a informação do time que está nessa posição.
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+var counter = 20;
+
+while(counter <= 30) {
+    console.log(counter);
+    counter++;
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +86,36 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+function convertToHex(color) {
+    switch(color) {
+        case 'black':
+        console.log('O hexadecimal para a cor preta é #000000.');
+        break;
+        case 'white':
+        console.log('O hexadecimal para a cor branca é #FFFFFF.');
+        break;
+        case 'red':
+        console.log('O hexadecimal para a cor vermelha é #FC0905.');
+        break;
+        case 'pink':
+        console.log('O hexadecimal para a cor rosa é #fc057c.');
+        break;
+        case 'blue':
+        console.log('O hexadecimal para a cor azul é #4705fc.');
+        break;
+        default:
+        console.log('Não temos o equivalente hexadecimal para ' + color + '.')
+    }
+}
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+convertToHex('black'); // O hexadecimal para a cor preta é #000000.
+convertToHex('pink'); // O hexadecimal para a cor rosa é #fc057c.
+convertToHex('red'); // O hexadecimal para a cor vermelha é #FC0905.
+convertToHex('blue'); // O hexadecimal para a cor azul é #4705fc.
+convertToHex('white'); // O hexadecimal para a cor branca é #FFFFFF.
+convertToHex('yellow'); // Não temos o equivalente hexadecimal para yellow.
+convertToHex('brown'); // Não temos o equivalente hexadecimal para brown.
+convertToHex('grey'); // Não temos o equivalente hexadecimal para grey.
